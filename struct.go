6f7eabee0f33f024c8867e9432b2cab818c43554
Lenfree Yeung <lenfree.yeung@gmail.com>
2018-08-24 15:09:14 +1000

    * Return health status of kafka-admin-service health endpoint * Add httptest for stub server
    CCI Build status for private repo
    
    Fix read and remove unneeded packages
    
    Add NewClient test case when client pass empty port
    
    Use port as int rather than string
    
    Fix doc
    
    Replace host and port with URL since this is endpoint is a
    web service to avoid confusion

--- struct.go
+++ struct.go
@@ -1,8 +1,12 @@
 package gokafkaesque
 
+// Topic have information about a Kafka topic.
 type Topic struct {
 	Meta `json:"response"`
 }
+
+// Meta includes Kafka topic config, partitions, replication
+// factor and name.
 type Meta struct {
 	Config            `json:"config"`
 	Partitions        int64  `json:"partitions"`
@@ -10,18 +14,22 @@ type Meta struct {
 	Name              string `json:"name"`
 }
 
+// Config contains a Kafka topic retention config in ms.
 type Config struct {
-	Config struct {
-		string `json:"retention.ms"`
-	}
+	RetentionMs  string `json:"retention.ms"`
+	SegmentBytes string `json:"segment.bytes"`
 }
 
+// Topics is a list of topic.
 type Topics []Topic
 
+// Health returns a response of OK.
 type Health struct {
 	Response string `json:"response"`
 }
 
+// ConfigMap contains additionalprop of a Kafka topic. Used to
+// updating existing Kafka topic.
 type ConfigMap struct {
 	AdditionalProp1 string `json:"additionalProp1"`
 	AdditionalProp2 string `json:"additionalProp2"`
