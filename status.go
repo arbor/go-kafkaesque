d90560c5b112db0d4028064a97a2b010025e3ca9
Lenfree Yeung <lenfree.yeung@gmail.com>
2018-08-28 13:57:25 +1000

    Move  status endpoint to its own file

---
+++ status.go
@@ -0,0 +1,27 @@
+package gokafkaesque
+
+import "fmt"
+
+// GetStatus returns status kafka-admin-service /health endpoint.
+func (client *Client) GetStatus() (Health, error) {
+	//func (client *Client) getHealth() string {
+	//return client.Rest.HostURL
+	resp, err := client.Rest.R().Get("/health")
+	if err != nil {
+		return Health{}, fmt.Errorf("ERROR: %s", err.Error())
+	}
+	if resp.StatusCode() >= 200 && resp.StatusCode() <= 299 {
+		var data Health
+		err := client.Rest.JSONUnmarshal(resp.Body(), &data)
+		if err != nil {
+			return Health{}, fmt.Errorf("ERROR: %s", err.Error())
+		}
+		return data, nil
+	}
+	return Health{}, fmt.Errorf("get status error: %v", resp.Status())
+}
+
+// GetHealth is a method that returns actual health status of "ok".
+func (h *Health) GetHealth() string {
+	return h.Response
+}
--- topics.go
+++ topics.go
@@ -4,30 +4,6 @@ import (
 	"fmt"
 )
 
-// GetStatus returns status kafka-admin-service /health endpoint.
-func (client *Client) GetStatus() (Health, error) {
-	//func (client *Client) getHealth() string {
-	//return client.Rest.HostURL
-	resp, err := client.Rest.R().Get("/health")
-	if err != nil {
-		return Health{}, fmt.Errorf("ERROR: %s", err.Error())
-	}
-	if resp.StatusCode() >= 200 && resp.StatusCode() <= 299 {
-		var data Health
-		err := client.Rest.JSONUnmarshal(resp.Body(), &data)
-		if err != nil {
-			return Health{}, fmt.Errorf("ERROR: %s", err.Error())
-		}
-		return data, nil
-	}
-	return Health{}, fmt.Errorf("get status error: %v", resp.Status())
-}
-
-// GetHealth is a method that returns actual health status of "ok".
-func (h *Health) GetHealth() string {
-	return h.Response
-}
-
 // GetTopics is a method that returns all Kafka topics.
 func (client *Client) GetTopics() (Topics, error) {
 	//func (client *Client) getHealth() string {
