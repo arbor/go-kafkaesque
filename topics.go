8e183cadfdfac0c865f9dff1572047b305e489bf
Issa Moussa <imoussa@arbor.net>
2018-08-29 20:10:29 +1000

    Add feature to get a topic information
    Return information about Topic like the topic configuration, number of partitions

--- topics.go
+++ topics.go
@@ -28,7 +28,7 @@ func (h *Health) GetHealth() string {
 	return h.Response
 }
 
-// GetTopics is a a method that returns all Kafka topics.
+// GetTopics is a method that returns all Kafka topics.
 func (client *Client) GetTopics() (Topics, error) {
 	//func (client *Client) getHealth() string {
 	//return client.Rest.HostURL
@@ -55,4 +55,23 @@ func (t Topics) Count() int {
 // Topics is a method that returns a slice of topics.
 func (t Topics) Topics() []string {
 	return t.Response.Topics
+}
+
+// GetTopic is a method that return a Kafka topic
+func (client *Client) GetTopic(topic string) (Topic, error) {
+	if len(topic) > 0 {
+		resp, err := client.Rest.R().Get("/topics/" + topic)
+		if err != nil {
+			return Topic{}, fmt.Errorf("ERROR: %s", err.Error())
+		}
+		if resp.StatusCode() >= 200 && resp.StatusCode() <= 299 {
+			var data Topic
+			err := client.Rest.JSONUnmarshal(resp.Body(), &data)
+			if err != nil {
+				return Topic{}, fmt.Errorf("ERROR: %s", err.Error())
+			}
+			return data, nil
+		}
+	}
+	return Topic{}, fmt.Errorf("Please provide a topic name\n")
 }
--- topics_test.go
+++ topics_test.go
@@ -87,3 +87,71 @@ func contains(ts Topics, t string) bool {
 	}
 	return false
 }
+
+func TestGetTopic(t *testing.T) {
+	var apiStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case "/topics/test_kafka-admin-service":
+			fmt.Fprint(w, fixture("topic.json"))
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer apiStub.Close()
+	config := NewConfig().SetURL(apiStub.URL).Build()
+	client := NewClient(config)
+	var data = []struct {
+		expectedName        string
+		expectedRetentionMs string
+		expectedPartitions  int64
+	}{
+		{"test_kafka-admin-service", "31536000000", 1},
+	}
+	for _, tt := range data {
+		r, err := client.GetTopic(tt.expectedName)
+		if err != nil {
+			t.Errorf("%v", err.Error())
+			t.FailNow()
+		}
+		if r.Response.Config.RetentionMs != tt.expectedRetentionMs {
+			t.Errorf("RetentionMS expected %v, got %v", tt.expectedRetentionMs, r.Response.Config.RetentionMs)
+		}
+		if r.Response.Partitions != tt.expectedPartitions {
+			t.Errorf("Partitions expected %v, got %v", tt.expectedPartitions, r.Response.Partitions)
+		}
+	}
+}
+
+func TestInvalidGetTopicName(t *testing.T) {
+	var apiStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case "/topics/test_kafka-admin-service":
+			fmt.Fprint(w, fixture("topic.json"))
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer apiStub.Close()
+	config := NewConfig().SetURL(apiStub.URL).Build()
+	client := NewClient(config)
+	var data = []struct {
+		expectedName        string
+		expectedRetentionMs string
+		expectedPartitions  int64
+	}{
+		{"", "", 0},
+	}
+	for _, tt := range data {
+		_, err := client.GetTopic(tt.expectedName)
+		if err == nil {
+			t.Errorf("%v", err.Error())
+			t.FailNow()
+		}
+	}
+}
